f989a69d9454d05241bf0496e7e9ffb5d59817b3
Yogesh-Oza <61262861+Yogesh-Oza@users.noreply.github.com>
2024-08-19 07:30:34 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './App.css';
+import Dashboard from "./Components/Dashboard";
+import NavBar from "./Components/NavBar";
+
+function App() {
+  return (
+    <>
+      <NavBar></NavBar>
+       <Dashboard></Dashboard>
+    </>
+   
+  );
+}
+
+export default App;
